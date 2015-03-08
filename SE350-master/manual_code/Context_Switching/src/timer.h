d631b2a269c61940216efbe68494a70b4cee7849
unknown <t7tong@ece-rtos24.NEXUS.UWATERLOO.CA>
2015-03-07 23:56:39 -0500

    adding timer.h file

---
+++ SE350-master/manual_code/Context_Switching/src/timer.h
@@ -0,0 +1,12 @@
+/**
+ * @brief timer.h - Timer header file
+ * @author Y. Huang
+ * @date 2013/02/12
+ */
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+/* initialize timer n_timer */
+extern uint32_t timer_init ( uint8_t n_timer );
+
+#endif /* ! _TIMER_H_ */
